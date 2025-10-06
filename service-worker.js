77d116e40e4d9a3c6b8c0619904f97e8de8ed964
Natylindatlgd <vitoria.pereira.nathalya@escola.pr.gov.br>
2025-10-06 08:34:43 -0300

    first commit

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install",event =>{
+    console.log("Service worker instalado")
+});
+
+self.addEventListener("activate",event =>{
+    console.log("service Worker ativado")
+});
+
+self.addEventListener("fetch",event =>{
+
+});
